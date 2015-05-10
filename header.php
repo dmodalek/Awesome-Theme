d027296c0585f803fbcc4983edf6a77c3c59c9f4
Dominic Modalek <dominic@modalek.ch>
2015-05-10 19:17:06 +0200

    Modalek Websolutions Ascii

--- header.php
+++ header.php
@@ -2,6 +2,21 @@
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<!--
+
+		Created by
+		 __  __           _       _      _     __          __  _               _       _   _
+		|  \/  |         | |     | |    | |    \ \        / / | |             | |     | | (_)
+		| \  / | ___   __| | __ _| | ___| | __  \ \  /\  / /__| |__  ___  ___ | |_   _| |_ _  ___  _ __  ___
+		| |\/| |/ _ \ / _` |/ _` | |/ _ \ |/ /   \ \/  \/ / _ \ '_ \/ __|/ _ \| | | | | __| |/ _ \| '_ \/ __|
+		| |  | | (_) | (_| | (_| | |  __/   <     \  /\  /  __/ |_) \__ \ (_) | | |_| | |_| | (_) | | | \__ \
+		|_|  |_|\___/ \__,_|\__,_|_|\___|_|\_\     \/  \/ \___|_.__/|___/\___/|_|\__,_|\__|_|\___/|_| |_|___/
+
+		Hi there, nice to meet you!
+
+		Visit our website at http://www.modalek.ch and send us an email :)
+
+		-->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 
